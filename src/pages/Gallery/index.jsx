be3a5b8236a2eed8888d97c1097fdbb319647d2c
Loic <loic.brassart@gmail.com>
2021-05-11 11:28:11 +0200

    Add Gallery page (use of before/after css pseudo-elements)

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -3,8 +3,14 @@ import { SGallery, SImg } from './style';
 export default function Gallery() {
   return (
     <SGallery>
-      Toto?
-      <SImg />
+      <SImg>
+        <img src="/img/backgrounds/1.jpg" alt="" />
+        <figcaption>Toto</figcaption>
+      </SImg>
+      <SImg>
+        <img src="/img/backgrounds/2.jpg" alt="" />
+        <figcaption>Toto</figcaption>
+      </SImg>
     </SGallery>
   );
 }
