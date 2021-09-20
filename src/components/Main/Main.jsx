727d10c65bf34b8aea9d8e8253f50141e9be637b
Hacene010 <hamrounehacene06@gmail.com>
2021-09-20 15:16:32 +0200

    created a main component with its style file and called it from app.jsx

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import SMain from './Style';
+
+export default function Main() {
+  return (
+    <SMain>
+      <div>
+        <h1>Main</h1>
+        <p>Hello I&apos;m in the main</p>
+      </div>
+    </SMain>
+  );
+}
