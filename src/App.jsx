3c8ce9c2962b257d939e8a1083d5842642e2684e
Hacene010 <hamrounehacene06@gmail.com>
2021-09-20 15:18:11 +0200

    updated the app.jsx components by calling the header, main and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import SApp from './style';
 
 function App() {
-  return <SApp />;
+  return (
+    <SApp>
+      <Header />
+      <Main />
+      <Footer />
+    </SApp>
+  );
 }
 
 export default App;
