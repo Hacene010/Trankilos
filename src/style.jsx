97e7be1ecf410757bc0040349c17c468deca3d93
Hacene010 <hamrounehacene06@gmail.com>
2021-09-21 15:37:00 +0200

    Added a font family to the app

--- src/style.jsx
+++ src/style.jsx
@@ -1,4 +1,7 @@
 import styled from 'styled-components';
 
-const SApp = styled.div``;
+const SApp = styled.div`
+  background-color: #f2f2f2;
+  font-family: 'Playfair Display', serif;
+`;
 export default SApp;
