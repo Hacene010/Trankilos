cd3ef195939058517f1fd330fe56d60eb348de73
Hacene010 <hamrounehacene06@gmail.com>
2021-09-21 15:38:44 +0200

    Created a header with basic inputs, logo and catch phrase

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,24 @@ import SHeader from './Style';
 export default function Header() {
   return (
     <SHeader>
-      <div>
-        <h1>Header</h1>
+      <section>
+        <div className="logo">
+          <img src="/assets/img/Trankilos.gif" alt="logoTrankilos" />
+        </div>
+        <div className="auth">
+          <input type="button" value="Inscription" />
+          <input type="button" value="Connexion" />
+        </div>
+      </section>
+
+      <div className="cacthPhrase">
+        <h1>
+          NE RÉFLICHISSEZ PLUS, <br />
+          <span>
+            NOUS LE FAISONS POUR VOUS
+            <img src="/assets/img/emoji.jpeg" alt="emoji" />
+          </span>
+        </h1>
       </div>
     </SHeader>
   );
--- src/components/Header/Style.jsx
+++ src/components/Header/Style.jsx
@@ -1,4 +1,53 @@
 import styled from 'styled-components';
 
-const SHeader = styled.div``;
+const SHeader = styled.div`
+  height: 30vh;
+
+  section {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .logo {
+    img {
+      height: 20vh;
+    }
+  }
+  .auth {
+    input {
+      font-family: 'Playfair Display', serif;
+      font-weight: 600;
+      border: none;
+      border-radius: 5px;
+      background-color: #fc4f4f;
+      color: #f2f2f2;
+      padding: 5px;
+      margin: 80px 30px 0px 0px;
+    }
+  }
+
+  h1 {
+    font-size: 2rem;
+    font-weight: 600;
+    text-align: center;
+    margin: 0.2rem 0 0 -10rem;
+
+    span {
+      margin-left: 25rem;
+    }
+
+    img {
+      width: 30px;
+      margin: 2px;
+      padding: 5px 0px 0px 5px;
+    }
+    @media (max-width: 720px) {
+      margin: 0;
+
+      span {
+        margin-left: 0;
+      }
+    }
+  }
+`;
 export default SHeader;
