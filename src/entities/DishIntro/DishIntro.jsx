7a92a3f6d103c01c79c068f2c515f2999626bf80
Hacene010 <hamrounehacene06@gmail.com>
2021-11-25 15:00:03 +0100

    Removed one dish from the DishIntro section

--- src/entities/DishIntro/DishIntro.jsx
+++ src/entities/DishIntro/DishIntro.jsx
@@ -14,18 +14,20 @@ export default function DishIntro() {
   return (
     <SDishIntro>
       <section className="container">
-        {dishes.map((dish) => {
-          return (
-            <article className="menus" key={dish.id}>
-              <div className="title_img">
-                <Link to={`/dishes/${dish.id}`}>
-                  <h3>{dish.name}</h3>
-                </Link>
-                <img src={dish.img} alt="dishImage" />
-              </div>
-            </article>
-          );
-        })}
+        {dishes
+          .map((dish) => {
+            return (
+              <article className="menus" key={dish.id}>
+                <div className="title_img">
+                  <Link to={`/dishes/${dish.id}`}>
+                    <h3>{dish.name}</h3>
+                  </Link>
+                  <img src={dish.img} alt="dishImage" />
+                </div>
+              </article>
+            );
+          })
+          .slice(0, 4)}
       </section>
 
       <hr />
